ccfe65a2923a6eb525344692c51b4097cdd4641f
kipfer <christoph.kattmann@googlemail.com>
2017-12-03 17:58:41 +0100

    Changes to api

--- server/api.py
+++ server/api.py
@@ -1,6 +1,6 @@
 import json
 import time
-# import datetime
+import datetime
 # import pprint
 
 from flask import Flask, request, jsonify
@@ -16,10 +16,17 @@ CLIENT = influxdb.InfluxDBClient(**credentials)
 
 
 def parse_timeInterval(timeInterval):
+    influxdb_time_format = '%Y-%m-%dT%H:%M:%SZ'
+    first_timestamp_today = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    first_timestamp_thisweek = first_timestamp_today - datetime.timedelta(days=first_timestamp_today.weekday())
+
+    start_of_today = first_timestamp_today.strftime(influxdb_time_format)
+    start_of_thisweek = first_timestamp_thisweek.strftime(influxdb_time_format)
+    
     shortcuts = {'last24h': 'time > now() - 24h',
                  'last7d': 'time > now() - 168h',
-                 'today': 'time > now() - 24h',
-                 'thisweek': 'time > now() - 168h'}
+                 'today': "time >= '"+start_of_today+"' and time < '"+start_of_today+"' + 24h", # explicit timestamp in influxdb queries have to be single-quoted
+                 'thisweek': "time >= '"+start_of_thisweek+"' and time < '"+start_of_thisweek+"' + 7d"}
     if timeInterval in shortcuts:
         parsed_timeInterval = shortcuts[timeInterval]
     else:
@@ -40,7 +47,7 @@ def build_query_string(query_dict):
 
     select_string += selected_string
 
-    # from_string = 'FROM ' + query_dict['device']
+    # FROM
     from_string = 'FROM ' + 'netzmonitor'
 
     # WHERE
@@ -52,25 +59,6 @@ def build_query_string(query_dict):
     query_string = ' '.join([select_string, from_string, where_string, groupby_string])
 
     return query_string
-
-
-@app.route('/api/hello', methods=['GET'])
-def hello():
-    return 'Hello World'
-
-
-@app.route('/api/write', methods=['POST'])
-def write_to_db():
-    data = request.get_json()
-
-    # Write data to InfluxDB database
-    CLIENT.write_points(data, time_precision='s')
-
-    # Additionally write latest json to file for quick retrieval of status
-    # address = data['tags']['address']
-    # with open()
-
-    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
 
 @app.route('/api/query', methods=['GET'])
@@ -91,7 +79,8 @@ def querydb():
         query_result = CLIENT.query(query_string, epoch='ms')  # ms plays nicely with Dygraphs
         print('Query Time', time.time() - query_starttime)
     except influxdb.exceptions.InfluxDBClientError:
-        print('DB not found?')
+        print('Error with query string ', end='')
+        print(query_string)
         raise
 
     try:
@@ -107,14 +96,26 @@ def querydb():
     return jsonify({'data':data, 'labels':labels})
 
 
+@app.route('/api/write', methods=['POST'])
+def write_to_db():
+    data = request.get_json()
+
+    # Write data to InfluxDB database
+    CLIENT.write_points(data, time_precision='s')
+
+    # Additionally write latest json to file for quick retrieval of status
+    # address = data['tags']['address']
+    # with open()
+
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+
+
 @app.route('/api/status', methods=['GET'])
 def get_status():
     # req = request.args.to_dict()
-    result = CLIENT.query('SELECT LAST(U1),U2,U3 from netzmonitor', epoch='ms')
+    result = CLIENT.query('SELECT LAST(U1),U2,U3,THDU1,THDU2,THDU3,I1,I2,I3,P1,P2,P3 from netzmonitor', epoch='ms')
     status = list(result.get_points())[0]
-
-    with open('lastStatus.json','w') as f:
-        f.write(json.dumps(status))
+    status['U1'] = status['last']
 
     return jsonify({'status':status})
 
