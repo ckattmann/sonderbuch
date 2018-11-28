f238d489b1a4bfb4764e6c70b9977667c41ad232
kanell <maltegerber@gmail.com>
2018-11-28 23:57:18 +0100

    restrict map zoom

--- website/src/main.js
+++ website/src/main.js
@@ -59,7 +59,7 @@ $(document).ready(function() {
 
         // L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {attribution: ''}).addTo(map);
         L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
-            maxZoom: 18, 
+            maxZoom: 18, minZoom: 3, 
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attribution">CARTO</a>'
         }).addTo(map);
 
